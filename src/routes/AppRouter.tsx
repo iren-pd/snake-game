4dc0bc2b0c1182493238fae3f90cb5bc1cff2a09
iren-pd <irenpd45@gmail.com>
2025-03-22 20:02:18 +0200

    Add error route

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import GamePage from "../pages/GamePage";
 import routes from "./routes";
@@ -9,6 +9,8 @@ const AppRouter = () => {
       <Routes>
         <Route path={routes.home} element={<HomePage />} />
         <Route path={routes.game} element={<GamePage />} />
+
+        <Route path="*" element={<Navigate to={routes.home} replace />} />
       </Routes>
     </Router>
   );
